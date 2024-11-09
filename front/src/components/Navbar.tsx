9a33576a040e8a5868b120b20bd8ab92e4b4ffcf
JC Ejercito <joshejercito01@gmail.com>
2024-11-09 18:53:14 +0800

    update: minor view adjustments on navbar

--- front/src/components/Navbar.tsx
+++ front/src/components/Navbar.tsx
@@ -6,26 +6,27 @@ interface NavbarProps {
 }
 
 const Navbar: React.FC<NavbarProps> = ({ balance }) => {
-  return (
-    <div className="navbar">
-      {/* Three bars icon */}
-      <div className="navbar__icon navbar__menu-icon">
-        <span className="bar"></span>
-        <span className="bar"></span>
-        <span className="bar"></span>
+    return (
+      <div className="navbar">
+        {/* Three bars icon and title */}
+        <div className="navbar__left">
+          <div className="navbar__menu-icon">
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+          </div>
+          <div className="navbar__title">FUN88</div>
+        </div>
+  
+        {/* Wallet icon, user icon, and balance */}
+        <div className="navbar__icons">
+          <div className="navbar__icon navbar__wallet-icon">💰</div>
+          <div className="navbar__balance">₱{balance.toFixed(2)}</div>
+          <div className="navbar__icon navbar__user-icon">👤</div>
+        </div>
       </div>
-
-      {/* Title */}
-      <div className="navbar__title">FUN88</div>
-
-      {/* Wallet icon, user icon, and balance */}
-      <div className="navbar__icons">
-        <div className="navbar__icon navbar__wallet-icon">💰</div>
-        <div className="navbar__icon navbar__user-icon">👤</div>
-        <div className="navbar__balance">₱{balance.toFixed(2)}</div>
-      </div>
-    </div>
-  );
-};
+    );
+  };
+  
 
 export default Navbar;
